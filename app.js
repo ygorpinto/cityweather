3028c9cec6b0ff152c76eb7e42ee2f77a7dd169f
Ygor Pinto <73406162+ygorpinto@users.noreply.github.com>
2021-02-02 17:04:49 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,20 @@
+const button = document.querySelector('#button')
+
+button.addEventListener('click', (e) => {
+    e.preventDefault()
+    const city = document.querySelector('#city').value
+    const Bring = async () => {
+        const response = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city}&lang=pt_br&units=metric&appid=9bea4fffe3d51d2da210f4269fa3a525`)
+        const data = await response.json();
+        console.log(data);
+        const body = document.querySelector('body')
+        const showWeather = document.createElement('div')
+        showWeather.id = "ShowWeather"
+        showWeather.innerHTML = `<p> <strong> Cidade :${data.name} </strong></p> <p><strong>Temperatura : ${Math.floor(data.main.temp)}º </strong></p>`
+        body.append(showWeather);
+    }
+
+    Bring();
+})
+
+
